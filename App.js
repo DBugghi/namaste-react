8be324f18e84e318d4accf95102ea3fd3e17097b
DBugghi <dibugghi@gmail.com>
2024-03-29 23:43:03 +0530

    Episode 02

--- App.js
+++ App.js
@@ -1,4 +1,7 @@
-const parent = React.createElement("div", {id:"parent"},React.createElement("div", {id:"child"}, [React.createElement("h1",{}, "I am an H1 tag"), React.createElement("h2",{}, "I am an H2 tag")] ));
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const parent = React.createElement("div", {id:"parent"},React.createElement("div", {id:"child"}, [React.createElement("h1",{}, "I am an H-ONE tag"), React.createElement("h2",{}, "I am an H2 tag")] ));
 
 console.log(parent);
 const root = ReactDOM.createRoot(document.getElementById("root"));
